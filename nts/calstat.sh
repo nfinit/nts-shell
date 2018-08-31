b416baaaeb5b3128cad1eb0e0298698899433dab
ict <ict@nfinit.systems>
2018-08-31 17:48:30 -0600

    Fixed calstat script printing nonsense string when no appointments were found

--- nts/calstat.sh
+++ nts/calstat.sh
@@ -7,9 +7,12 @@
 # Check if calcurse has been used first #
 if [ -d "$HOME/.calcurse" ]; then
 	# Only the default calendar will be printed #
-	tput setaf 006
-	echo -n "Today's appointments for "
-	calcurse -a
-	echo -ne "\n"
-	tput sgr0
+	CALOUT=$(calcurse -a)
+	if [ ! -z $CALOUT ]; then
+		tput setaf 006
+		echo -n "Today's appointments for "
+		calcurse -a
+		echo -ne "\n"
+		tput sgr0
+	fi
 fi
