477c3e0b7adbcf106a88c261b176d1ccc9488406
ict <ict@nfinit.systems>
2018-08-31 19:44:43 -0600

    Added messages directory, fixed spacing issues in some sim scripts

--- sim/focal15.sh
+++ sim/focal15.sh
@@ -2,15 +2,15 @@
 #################################################
 #  FOCAL15: SimH emulated FOCAL-15 environment  #
 #################################################
- 					      	#
-SIM="/usr/local/bin/pdp15 -q"		      	#
-CONFIG=/usr/share/simh/configs/pdp15-focal.simh	#
-MSG=/usr/share/simh/msg/focal15.msg		#
-						#
+                                                #
+SIM="/usr/local/bin/pdp15 -q"                   #
+CONFIG=/usr/share/simh/configs/pdp15-focal.simh #
+MSG=/usr/share/simh/msg/focal15.msg             #
+                                                #
 #################################################
-#	DO NOT MODIFY BELOW THIS POINT		#
+#        DO NOT MODIFY BELOW THIS POINT         #
 #################################################
-[[ -f $MSG ]] && cat $MSG 			#
-echo -n "(send ^E to halt/exit)"		#
-$SIM $CONFIG					#
+[[ -f $MSG ]] && cat $MSG                       #
+echo -n "(send ^E to halt/exit)"                #
+$SIM $CONFIG                                    #
 #################################################
